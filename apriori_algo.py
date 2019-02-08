54fc7392a9befd064936b2ed5251d62ecd9a37b9
Tukanuk <benjamin@idavidson.ca>
2019-02-07 21:57:42 -0500

    Added basic timing for function

--- apriori_algo.py
+++ apriori_algo.py
@@ -1,23 +1,36 @@
 import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import apriori
+import time
 
-data_lines = open('data/data.txt').readlines()
+def print_timing(func):
+    def wrapper(*arg):
+        t1 = time.time()
+        res = func(*arg)
+        t2 = time.time()
+        print ("%s took %0.3f ms" % (func.__name__ , (t2-t1)*1000.0))
+        return res
+    return wrapper
 
-dataset = []
-for entry in data_lines:
-    cells = []
-    cells = entry.split(" ")
-    dataset.append(cells[:-1])
+@print_timing
+def alg():
+    data_lines = open('data/data.txt').readlines()
+    # data_lines = open('retail.txt').readlines()
 
-te = TransactionEncoder()
-te_ary = te.fit(dataset).transform(dataset)
+    dataset = []
+    for entry in data_lines:
+        cells = []
+        cells = entry.split(" ")
+        dataset.append(cells[:-1])
 
-df = pd.DataFrame(te_ary, columns=te.columns_)
+    te = TransactionEncoder()
+    te_ary = te.fit(dataset).transform(dataset)
 
-frequent_items = apriori(df, min_support=0.05, use_colnames=True)
+    df = pd.DataFrame(te_ary, columns=te.columns_)
 
-print(frequent_items)
+    frequent_items = apriori(df, min_support=0.05, use_colnames=True, max_len=2 )
 
+    print(frequent_items)
 
-
+if __name__ == "__main__":
+    alg()
